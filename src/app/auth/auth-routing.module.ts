14a436648d4fc6756c85528b30eccbab5b14b339
tw1ttt3r <tw1ttt3r@gmail.com>
2021-09-26 04:04:23 -0500

    feat: clean auth module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,8 @@
-import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { AuthRoutingModule } from './auth-routing.module';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
-import { ReactiveFormsModule} from '@angular/forms'
+import { UserService } from '../core/services/user.service';
 
 
 @NgModule({
@@ -13,16 +15,12 @@ import { ReactiveFormsModule} from '@angular/forms'
     LoginComponent,
     RegisterComponent
   ],
-  exports: [
-    LoginComponent,
-    RegisterComponent,
-  ],
   imports: [
     CommonModule,
-    RouterModule,
+    AuthRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule
-  ]
+  ],
 })
 export class AuthModule { }
