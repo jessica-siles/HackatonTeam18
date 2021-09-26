be85da41084868b356db3a7a57d42a0ce6fecb4c
tw1ttt3r <tw1ttt3r@gmail.com>
2021-09-26 15:02:49 -0500

    feat: refactored use service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,42 @@ export class UserService {
   constructor() { }
 
   user = {
-    loggedIn: false
+    loggedIn: false,
+    loginError: false,
+    profile: {},
+    uid: '',
+    token: { }
   }
 
   getLoggedStatus(): boolean {
     return this.user.loggedIn;
+  }
+
+  setLoggenStatus(status: boolean) {
+    this.user.loggedIn = status;
+  }
+
+  setUserProfile(data: any) {
+    this.user.profile = data;
+  }
+
+  setToken(token: any) {
+    this.user.token = token;
+  }
+
+  setUid(uid: string) {
+    this.user.uid = uid;
+  }
+
+  getUid() {
+    return this.user.uid;
+  }
+
+  setErrorLogin(error: boolean) {
+    this.user.loginError = error;
+  }
+
+  errorLogin() {
+    return of(this.user.loginError)
   }
 }
