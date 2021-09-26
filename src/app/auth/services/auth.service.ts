26452ec32129d870925ab3c93acffb14d68cf111
tw1ttt3r <tw1ttt3r@gmail.com>
2021-09-26 10:43:05 -0500

    feat: added register and createAccount methods

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { setDoc, doc } from 'firebase/firestore';
+import { FirestoreService } from 'src/app/core/services/firestore.service';
 
 import { FirebaseService } from '../../core/services/firebase.service';
 
@@ -13,22 +15,35 @@ import { AccountModel } from '../models/account.model';
 })
 export class AuthService {
 
-  constructor(private fbService: FirebaseService, private router: Router) { }
+  constructor(
+    private fbService: FirebaseService,
+    private router: Router,
+    private fstoreService: FirestoreService
+  ) { }
 
   createAccount(data: AccountModel) {
     console.log(data);
     createUserWithEmailAndPassword(this.fbService.auth, data.username, data.password)
       .then(userCredential => {
-        console.log(userCredential)
+        this.registerProfile(data, userCredential.user.uid);
       })
       .catch( err => {
-        console.log(err);
+        this.router.navigate(['/', 'login'])
       })
-    this.router.navigate(['/', 'login'])
   }
 
-  registerProfile(data: AccountModel) {
-    // this.fbService.createProfile(data);
+  async registerProfile({ username, password, linkedin, country, description, rol, repository }: AccountModel, uid: string) {
+      await setDoc(doc(this.fstoreService.getFirestore(), 'usuario', uid),   {
+        username,
+        password,
+        linkedin,
+        country,
+        description,
+        rol, 
+        [rol === 'user' ? 'repository' : 'webpage']: repository
+      })
+      .then( res => this.router.navigate(['/', 'confirmation'], { queryParams: { status: true, error: ''}}))
+      .catch( err => this.router.navigate(['/', 'confirmation'], { queryParams: { status: true, error: err}}));
   }
 
 }
