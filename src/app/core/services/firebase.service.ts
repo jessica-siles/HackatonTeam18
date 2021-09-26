62f4343578af8d8a027b7656c75308d1018816ff
tw1ttt3r <tw1ttt3r@gmail.com>
2021-09-26 06:14:24 -0500

    feat: configured firebase service

--- src/app/core/services/firebase.service.ts
+++ src/app/core/services/firebase.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 
-import 'firebase/auth';
-import fb from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
 import { environment } from '../../../environments/environment';
 
-const app = fb.initializeApp(environment.FirebaseConfig);
-const auth = app.auth();
+const app = initializeApp(environment.firebaseConfig);
+const auth = getAuth();
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  auth = auth;
   app = app;
+  auth = auth;
 
   constructor() { }
 }
