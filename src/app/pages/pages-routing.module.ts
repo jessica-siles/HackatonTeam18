e036ba01b3a3e095a7ebbefe2b6bab1d7ff3e457
jessi siles <silesjessica@hotmail.com>
2021-09-26 12:12:47 -0300

    añadir rules

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -11,7 +11,7 @@ const routes:Routes = [
     path: '',
     component: PagesComponent,
     canActivate: [AuthGuard],
-    children: [
+    children:[       
       {
         path: 'bootcamps',
         component: BootcampsComponent,
