f48aad16b663818adbb3f24f3465ad6bfaa7c593
moche123 <mmiguel@unprg.edu.pe>
2021-09-26 09:32:57 -0500

    refactor ya tiene una cuenta?

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 
 export const environment = {
    production: false,
+   firebaseConfig : {
+     apiKey: "AIzaSyCDCqUHpCwftxEKDgt9rokKBr1WBvdfAJY",
+     authDomain: "hackatong-14.firebaseapp.com",
+     projectId: "hackatong-14",
+     storageBucket: "hackatong-14.appspot.com",
+     messagingSenderId: "792533135763",
+     appId: "1:792533135763:web:6b0a31a702dcda0f44b866"
+   }
 };
