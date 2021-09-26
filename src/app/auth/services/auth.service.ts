953250d3018c6a3a75f06aa8a9d50cdfee93c79e
tw1ttt3r <tw1ttt3r@gmail.com>
2021-09-26 06:14:55 -0500

    chore: added createAccouny method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { FirebaseService } from '../../core/services/firebase.service';
+
+import { AccountModel } from '../models/account.model';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private fbService: FirebaseService) { }
+
+  createAccount(data: AccountModel) {
+    console.log(data)
+  }
+
 }
