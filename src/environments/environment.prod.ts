53ee817e3b6ce305721111f990ba949c716cd96a
unknown <vic_ortiz20@hotmail.es>
2021-09-26 19:27:59 -0500

    .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    iKey: "AIzaSyCDCqUHpCwftxEKDgt9rokKBr1WBvdfAJY",
+    thDomain: "hackatong-14.firebaseapp.com",
+    ojectId: "hackatong-14",
+    orageBucket: "hackatong-14.appspot.com",
+    ssagingSenderId: "792533135763",
+    pId: "1:792533135763:web:6b0a31a702dcda0f44b866"
+  }
 }
