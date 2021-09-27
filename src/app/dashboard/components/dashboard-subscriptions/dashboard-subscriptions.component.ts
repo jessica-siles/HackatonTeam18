13d6ae5b5ce09a754065497e575da5ecfa7c7f75
jessi siles <silesjessica@hotmail.com>
2021-09-26 21:15:06 -0300

    agregado de dashboard subscription

--- src/app/dashboard/components/dashboard-subscriptions/dashboard-subscriptions.component.ts
+++ src/app/dashboard/components/dashboard-subscriptions/dashboard-subscriptions.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Empresas } from 'src/app/auth/models/empresa.model';
+import { AccountModel } from 'src/app/auth/models/account.model';
 @Component({
   selector: 'app-dashboard-subscriptions',
   templateUrl: './dashboard-subscriptions.component.html',
   styleUrls: ['./dashboard-subscriptions.component.scss']
 })
-export class DashboardSubscriptionsComponent implements OnInit {
+export class DashboardSubscriptionsComponent {
+  empresasArray: Empresas[] = [
+    {nombre:'Google',descripcion:'Bootcamp este fin de semana del 25 al 16 de octubre'},
+    {nombre:'Hk',descripcion:'Bootcamp este fin de semana del 25 al 16 de octubre'},
+    {nombre:'Teams',descripcion:'Bootcamp este fin de semana del 25 al 16 de octubre'},
+    {nombre:'Studio',descripcion:'Bootcamp este fin de semana del 25 al 16 de octubre'},
+  ];
+  usuariosArray: AccountModel[] = [
+    {username: 'Jose',
+      password: '123645',
+      password2: '123645',
+      linkedin: 'www.com',
+      country: 'españa',
+      repository: 'github',
+      description: 'soy jose',
+      rol: 'usuario'}
+    
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  validRoles = ['user', 'company']
+  logueado :string = 'user'
 }
